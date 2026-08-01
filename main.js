f332d8e5bb0cee3ab1ea26db52beb7d80995f602
R4mBLe_ <misharad2008@gmail.com>
2026-08-02 00:18:13 +0300

    update app

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3091;
 app.use(express.urlencoded({ extended: true }));
 app.post("/send_message", (req, res) => {
-  console.log(req.body);
+  console.log(req);
   //    const amoDomain = this.configService.get<string>('AMO_DOMAIN');
   //     const headers = {
   //       Authorization: `Bearer ${this.configService.get<string>('AMO_TOKEN')}`,
